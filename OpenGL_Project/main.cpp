5f35c7c2e3b637e3ade08701c1a4886c63795020
smnpenn <smnpenn@gmail.com>
2024-01-01 19:03:11 +0100

    project setup with GLAD, updated gitignore

--- OpenGL_Project/main.cpp
+++ OpenGL_Project/main.cpp
@@ -1,37 +1,68 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-int main(void)
+using namespace std;
+
+//Forward Declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
 {
-    GLFWwindow* window;
+    //init glfw (OpenGL Version: 4.6, Core Profile)
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    //create window context
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Name TBD", NULL, NULL);
+    if (window == NULL)
     {
+        cout << "Failed to create GLFW window" << endl;
         glfwTerminate();
         return -1;
     }
-
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    //initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        cout << "Failed to initialize GLAD" << endl;
+        return -1;
+    }
+
+    //configure viewport and set callback function for resizing
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //rendering loop
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
+    //cleans all GLFW ressources
     glfwTerminate();
     return 0;
+}
+
+//function to resize viewport, when window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+//handles all user input
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
